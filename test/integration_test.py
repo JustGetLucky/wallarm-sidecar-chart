d6d8977e4cb78c131552eb1a91815c7d40eec82f
Evgeniy Dmitriev <edmitriev@wallarm.com>
2022-06-21 19:33:22 +0700

    typo

--- test/integration_test.py
+++ test/integration_test.py
@@ -5,7 +5,7 @@ import requests
 import logging
 from time import sleep
 
-LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 PATCHES_PATH = 'kustomize/patches'
 ALLOWED_HTTP_PATH = '/get'
@@ -19,37 +19,37 @@ for patchPath in os.listdir(PATCHES_PATH):
 class Helpers:
     @staticmethod
     def subprocess_run(cmd: str) -> subprocess.CompletedProcess:
-        LOGGER.info(f'Command: {cmd}')
+        logger.info(f'Command: {cmd}')
         completed_process = subprocess.run(cmd, capture_output=True, text=True, shell=True)
-        LOGGER.info(f'Finished with exit code: {completed_process.returncode}')
+        logger.info(f'Finished with exit code: {completed_process.returncode}')
 
         return completed_process
 
     @staticmethod
     def create_namespace(namespace: str) -> subprocess.CompletedProcess:
         cmd = f'kubectl create namespace {namespace}'
-        LOGGER.info('Create namespace ...')
+        logger.info('Create namespace ...')
 
         return Helpers.subprocess_run(cmd)
 
     @staticmethod
     def create(path: str, namespace: str) -> subprocess.CompletedProcess:
         cmd = f'kubectl create -k {path}/ --namespace {namespace}'
-        LOGGER.info('Create configuration ...')
+        logger.info('Create configuration ...')
 
         return Helpers.subprocess_run(cmd)
 
     @staticmethod
     def wait(namespace: str, timeout: str = '180s') -> subprocess.CompletedProcess:
         cmd = f'kubectl wait --for=condition=Ready pods --all --timeout={timeout} --namespace {namespace}'
-        LOGGER.info('Wait for all Pods will be ready ...')
+        logger.info('Wait for all Pods will be ready ...')
 
         return Helpers.subprocess_run(cmd)
 
     @staticmethod
     def delete_namespace(namespace: str) -> subprocess.CompletedProcess:
         cmd = f'kubectl delete namespace {namespace}'
-        LOGGER.info('Delete namespace ...')
+        logger.info('Delete namespace ...')
         return Helpers.subprocess_run(cmd)
 
 
@@ -90,11 +90,11 @@ class TestMainFunctionality:
         assert wait.returncode == 0, wait.stderr
 
         sleep(5)
-        LOGGER.info(f'Performing allowed request: {allowed_url} ...')
+        logger.info(f'Performing allowed request: {allowed_url} ...')
         allowed_request = requests.get(allowed_url)
         assert allowed_request.status_code == 200
 
-        LOGGER.info(f'Performing forbidden request: {forbidden_url} ...')
+        logger.info(f'Performing forbidden request: {forbidden_url} ...')
         forbidden_request = requests.get(forbidden_url)
         assert forbidden_request.status_code == 403
 
@@ -103,5 +103,5 @@ class TestMainFunctionality:
         config = {}
         yield config
         namespace = config.get('namespace')
-        LOGGER.info('Teardown ...')
+        logger.info('Teardown namespace ...')
         helpers.delete_namespace(namespace)
